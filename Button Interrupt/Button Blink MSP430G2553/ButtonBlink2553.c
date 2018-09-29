3a6dc9a7746e20ec931f61c57668fa05bc2ee498
Johnsons8 <35497793+Johnsons8@users.noreply.github.com>
2018-09-28 22:56:23 -0400

    updated comments

--- Button Interrupt/Button Blink MSP430G2553/ButtonBlink2553.c
+++ Button Interrupt/Button Blink MSP430G2553/ButtonBlink2553.c
@@ -1,49 +1,11 @@
-//#include <msp430.h>
-//#include <msp430G2553.h>
-//
-///*
-// * main.c
-// */
-//
-//void main(void)
-//{
-//    WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog timer
-//
-//            P1REN |= 0x08;                          //Turn on PullUp on????? How do you turn it off?
-//
-//            P1DIR &= ~BIT3;                         // Push Port 1 P1.3 (push button) as input
-//            P1REN &= ~0x08;                         // Pull down resistor???
-//            P1DIR |= BIT0;
-//            P1DIR |= BIT6; // Set P1.6 (LED) to output direction
-//            P1SEL &= ~(BIT3 | BIT6);                // Select Port 1 P1.3 (push button)
-//
-//            P1OUT |= BIT0;
-//            P1OUT &= ~BIT6;   // Set the LED off
-//
-//
-//            while( 1 ) {
-//                int value = P1IN & BIT3;
-//                    if( value == 0){         // Push button down when bit 3 == 0
-//                            P1OUT |= BIT6;          // Set LED on when button down
-//                            P1OUT &= ~BIT0;
-//            }else{
-//                            P1OUT &= ~BIT6;         // Set LED off when button off
-//                            P1OUT |= BIT0;
-//            }
-//
-//
-//            }
-//} // CCR0_ISR
-/**
- * main.c
- */
+
 #include <msp430.h>
 #include <msp430g2553.h>
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR |= BIT0;                            // Set pin 1.0 to output
-  P1DIR |= BIT6;                            // Set pin 1.6 to output
+  P1DIR |= BIT0;                            // Set pin 1.0 to output (Green LED)
+  P1DIR |= BIT6;                            // Set pin 1.6 to output (Red LED)
   P1IE |=  BIT3;                            // Enable interupt on pin 1.3
   P1IES |= BIT3;                            // Detect falling edge on button press
   P1IFG &= ~BIT3;                           // Clear Flag
@@ -53,7 +15,7 @@ int main(void)
   __bis_SR_register(LPM4_bits + GIE);       // Enter LPM4 w/interrupt
 }
 
-#pragma vector=PORT1_VECTOR
+#pragma vector=PORT1_VECTOR                 //Interrupt when flag is triggered aka if button is pressed
 __interrupt void Port_1(void)
 {
   P1OUT ^= BIT6;                            // Toggle Red LED
