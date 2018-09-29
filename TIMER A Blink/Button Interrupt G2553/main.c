d4b95a7b3182d7f653f46e837d2385bd209fd780
Johnsons8 <35497793+Johnsons8@users.noreply.github.com>
2018-09-28 22:38:31 -0400

    Uploaded TIMER A Blink

---
+++ TIMER A Blink/Button Interrupt G2553/main.c
@@ -0,0 +1,65 @@
+#include <msp430.h>
+#include <msp430G2553.h>
+
+/*
+ * main.c
+ */
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P1DIR |= 0x01;                            // P1.0 output
+  P1DIR |= BIT6;
+  P1SEL &= ~(BIT3 | BIT6);
+  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+  CCR0 = 50000;
+  TACTL = TASSEL_2 + MC_1 + ID_3;                  // SMCLK, upmode
+  P1OUT |= BIT0;
+  P1OUT &= ~BIT6;
+  P1IFG &= ~BIT3;
+  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+
+{
+    int value = P1IN & BIT3;
+           if( value == 0){
+               CCR0 = 50000;
+           }else{
+               CCR0 = 15000;
+
+           }
+  P1OUT ^= 0x01;                            // Toggle P1.0
+  P1OUT ^= BIT6;                            // Toggle P1.6
+}
+//#include <msp430.h>
+//#include <msp430G2553.h>
+//
+///*
+// * main.c
+// */
+//int main(void)
+//{
+//  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+//  P1DIR |= 0x01;                            // P1.0 output
+//  P1DIR |= BIT6;
+//  P1SEL &= ~(BIT3 | BIT6);
+//  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+//  CCR0 = 50000;
+//  TACTL = TASSEL_2 + MC_1 + ID_3;                  // SMCLK, upmode
+//  P1OUT |= BIT0;
+//  P1OUT &= ~BIT6;
+//  //P1IFG &= ~BIT3;
+//  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
+//}
+//
+//// Timer A0 interrupt service routine
+//
+//#pragma vector=TIMER0_A0_VECTOR
+//__interrupt void Timer_A (void)
+//{
+//
+//  P1OUT ^= 0x01;                            // Toggle P1.0
+//  P1OUT ^= BIT6;                            // Toggle P1.6
+//}
